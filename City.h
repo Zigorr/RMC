5bc13ce37d3534d0000b1943560bd3e6b7899245
omar-7salama <152609164+omar-7salama@users.noreply.github.com>
2024-04-28 21:29:57 +0300

    edge

--- City.h
+++ City.h
@@ -7,9 +7,16 @@ using namespace std;
 class City
 {
 	public:
-	virtual void Input() = 0;
-	virtual string getName() const = 0;
-	virtual vector<pair<string, double>> getConnectedCities() const = 0;
+	// void Input() = 0;
+	//virtual string getName() const = 0;
+	 //vector<pair<string, double>> getConnectedCities() const = 0;
+	
+
+	City(const string cityName);
+	
+	string getName() const; 
+	
+
 protected:
 	string name;
 	vector<pair<string, double>> connectedCities;
--- main.cpp
+++ main.cpp
@@ -2,7 +2,15 @@
 using namespace std;
 int main() {
 
-	return 0;
+    if (lines.empty()) {
+        cerr << "Error: No lines read from input file." << endl;
+        return 0; // Exit with error code
+    }
+
+    // Display lines read from input file
+    cout << "Lines read from " << inputFilename << ":" << endl;
+    for (const string& line : lines) {
+        cout << line << endl;
 }
 
 
